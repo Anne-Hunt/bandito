f35ff6b4971ae261c24d9f687df5c72fe8ba1204
Anne Hunt <acathunt@gmail.com>
2024-07-22 21:53:19 -0600

    updated file structure

---
+++ pages/about.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+
+<template>
+<div>
+    <h2>About</h2>
+    <p>Welcome to Bandito, the premier shopping site for all of raccoonkind.</p>
+    <h3>About Us</h3>
+    <p><strong>What is Bandito?</strong></p>
+    <p>Every raccoon finds more trash than they can consume. Bandito is a place where excess trash goes to be sold to raccoons with premium taste in goods.</p>
+    <p><strong>Who runs Bandito?</strong></p>
+    <p>Bandito is solely run by raccoons. We once hired an oppossum but he was too anxious to complete much of the work in such a chaotic environment.</p>
+    <p><strong>Where are your goods? How do I get them?</strong></p>
+    <p>We ship everything to your nearest dumpster. Shipping times vary and are at best unreliable, but we try our best to work within the system the primates have left us with.</p>
+    <p><strong>Are you a joke?</strong></p>
+    <p>NO! We never joke about dumpsters, raccoon rights, or the value of our wares.</p>
+</div>
+</template>
+
+
+<style scoped>
+h2, h3{
+    margin-bottom: 20px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+
+<template>
+<div>
+    <h2>Home</h2>
+    <p>Welcome to Bandito, the premier shopping site for all of raccoonkind.</p>
+</div>
+</template>
+
+
+<style scoped>
+h2{
+    margin-bottom: 20px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
