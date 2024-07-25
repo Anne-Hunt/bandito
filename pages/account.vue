7b0111e0bda1b385a180c9bba2eb3a0607162294
Anne Hunt <acathunt@gmail.com>
2024-07-25 15:37:05 -0600

    continued attempts at Auth0

--- pages/account.vue
+++ pages/account.vue
@@ -1,33 +1,35 @@
 <script setup>
 definePageMeta({
-    layout: 'account'
+    layout: 'account',
+    middleware: ["auth"]
 })
 
-useHead({ 
+useHead({
     title: 'Bandito - Your Account'
 })
 </script>
 
 
 <template>
-<div>
-    <h2>Welcome!</h2>
-    <p>Welcome to Bandito, the premier shopping site for all of raccoonkind.</p>
-</div>
-<div class="card">
-    <div v-if="account" class="">Hello!</div>
-    <div v-else>
-        <p class="btn">Log In</p>
+    <div>
+        <h2>Welcome!</h2>
+        <p>Welcome to Bandito, the premier shopping site for all of raccoonkind.</p>
     </div>
-</div>
+    <div class="card">
+        <div v-if="account" class="">Hello!</div>
+        <div v-else>
+            <p class="btn">Log In</p>
+        </div>
+    </div>
 </template>
 
 
 <style scoped>
-h2{
+h2 {
     margin-bottom: 20px;
 }
-p{
+
+p {
     margin: 20px 0;
 }
 </style>
