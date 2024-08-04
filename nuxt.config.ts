9c68ec8083f1ab5682c6da27209bbeb898f5a87e
Anne Hunt <acathunt@gmail.com>
2024-08-03 21:27:52 -0600

    repaired config, added node modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', "nuxt-mdi", "nuxt-server-utils"],
+  modules: ['@nuxtjs/tailwindcss', "nuxt-mdi"],
   app: {
     head: {
       title: 'Bandito',
