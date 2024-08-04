77e7819af97c0a3bf0d0c77e59d1478da574b5f2
Anne Hunt <acathunt@gmail.com>
2024-08-03 21:56:18 -0600

    updated homepage

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,20 @@ useHead({
 <div>
     <h2>Home</h2>
     <p>Welcome to Bandito, the premier shopping site for all of raccoonkind.</p>
+    <div class="categories">
+        <h3>Products</h3>
+        <div class="list">
+            <div class="type">
+                <h5>Dumpster Goods</h5>
+            </div>
+            <div class="type">
+                <h5>Disguises</h5>
+            </div>
+            <div class="type">
+                <h5>Accessories</h5>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
