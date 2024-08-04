715ee6d9095cd4dbe97962e271da5b8ca0f1dc8f
Anne Hunt <acathunt@gmail.com>
2024-08-04 13:10:28 -0600

    homepage images set

--- pages/index.vue
+++ pages/index.vue
@@ -8,28 +8,28 @@ useHead({
 <template>
 
 <div class="px-10">
-    <h2 class="text-xl font-bold">Home</h2>
+    <h2 class="text-3xl font-bold">Home</h2>
     <p>Welcome to Bandito, the premier shopping site for all of raccoonkind.</p>
     <div class="categories">
-        <h3 class="text-xl font-semibold">Products</h3>
+        <h3 class="text-2xl font-semibold mb-5">Products</h3>
         <div class="">
-            <h4 class="text-lg">Special Sale on Food</h4>
-            <div class="sale">
+            <div class="sale rounded-md shadow-lg flex content-end p-5">
+                <h4 class="text-4xl font-bold text-white">Special Sale on Food</h4>
             </div>
         </div>
         <div class="list">
             <div class="grid grid-cols-3 gap-5">
                 <div class="p-5">
                     <h5 class="text-lg font-bold">Dumpster Goods</h5>
-                    <img src="https://balenciaga.dam.kering.com/m/34e56aaf423b3169/eCom-716897210JA9065_F.jpg" alt="bag of trash">
+                    <img class="rounded-md shadow-md" src="https://balenciaga.dam.kering.com/m/34e56aaf423b3169/eCom-716897210JA9065_F.jpg" alt="bag of trash">
                 </div>
                 <div class="p-5">
                     <h5 class="text-lg font-bold">Disguises</h5>
-                    <img src="https://i5.walmartimages.com/seo/GROUCHO-DISGUISE-SET-GLASS-MUSTACHE_b1e55507-91cd-416e-a6b7-f9898433aa6f.1554f89514ecfcff9b473250baf3cf81.jpeg" alt="mustache">
+                    <img class="rounded-md shadow-md" src="https://i5.walmartimages.com/seo/GROUCHO-DISGUISE-SET-GLASS-MUSTACHE_b1e55507-91cd-416e-a6b7-f9898433aa6f.1554f89514ecfcff9b473250baf3cf81.jpeg" alt="mustache">
                 </div>
                 <div class="p-5">
                     <h5 class="text-lg font-bold">Accessories</h5>
-                    <img src="https://www.dollartree.com/ccstore/v1/images/?source=/file/v2708670677486631216/products/219796.jpg" alt="Pinwheel">
+                    <img class="rounded-md shadow-md" src="https://www.dollartree.com/ccstore/v1/images/?source=/file/v2708670677486631216/products/219796.jpg" alt="Pinwheel">
                 </div>
             </div>
         </div>
