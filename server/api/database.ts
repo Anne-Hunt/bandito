b193af30a28d247664ccc813da8ae466b209940c
Anne Hunt <acathunt@gmail.com>
2024-08-07 17:51:26 -0600

    created api request files

--- server/api/database.ts
+++ server/api/database.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 export default async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(useRuntimeConfig().apiSecret.MONGO_URL)
+        await mongoose.connect(useRuntimeConfig().apiSecret.MONGO_URI)
         console.log('MongoDB is in the house.')
     } catch (error) {
         return createError({ statusCode: 500, statusMessage: 'Unable to connect to MongoDB or get response.' })
